eda5adffd7e9fefa71f4099ad7c56401d02e9c4d
Manik <abdul.halim@portonics.com>
2023-08-23 20:54:20 +0600

    L-4 Categories component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { User } from "@prisma/client"
-import Container from "../Container"
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
+import Container from "../Container"
+import Categories from "./Categories"
+
+import { SafeUser } from "@/app/types"
 
 interface NavbarProps {
-    currentUser?: User | null
+    currentUser?: SafeUser | null
 }
 const Navbar: React.FC<NavbarProps> = ({
     currentUser
@@ -23,6 +25,7 @@ const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories/>
         </div>
     )
 }
